9ac45e2582919d2a029cee3006dc9c202390f7d8
Sasha Mainardi <sashamainardi4@gmail.com>
2022-03-30 01:18:41 +0200

    fixed bug in confermation delete JS, COMPLETED BONUS VERSION

--- public/js/confirmation-delete.js
+++ public/js/confirmation-delete.js
@@ -2,7 +2,7 @@ const deleteForms = document.querySelectorAll('.delete-form');
 
 deleteForms.forEach(form => {
     form.addEventListener('submit', (e) => {
-        e.preventDefault;
+        e.preventDefault();
         const valid = confirm('Vuoi eleminare il post?');
         if (valid) e.target.submit();
     });
