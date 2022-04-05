9020daeed8fc81ddbbf3d71b0d12a4373601c036
Sasha Mainardi <sashamainardi4@gmail.com>
2022-04-05 23:19:38 +0200

    corrected UsersTableSeeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,16 +1,12 @@
 <?php
 
-use App\Models\Category;
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
-use App\Models\Post;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 use App\User;
+use Faker\Generator as Faker;
 
 
 
-class PostsTableSeeder extends Seeder
+class UsersTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -19,21 +15,21 @@ class PostsTableSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $user_ids = User::pluck('id')->toArray();
-        $category_ids = Category::pluck('id')->toArray();
+        $user = new User();
+        $user->name = 'Sasha';
+        $user->email = 'sasha@new.it';
+        $user->password = bcrypt('password');
 
-        for ($i = 0; $i < 4; $i++) {
-            $post = new Post();
+        $user->save();
 
-            $post->title = $faker->text(20);
-            $post->content = $faker->sentence();
-            $post->image = $faker->imageUrl(250, 250);
-            $post->slug = Str::slug($post->title, '-');
-            $post->category_id = Arr::random($category_ids);
-            $post->user_id = Arr::random($user_ids);
+        for ($i = 0; $i < 5; $i++) {
+            $new_user =  new User();
 
+            $new_user->name = $faker->firstName();
+            $new_user->email = $faker->email();
+            $new_user->password = bcrypt($faker->password(5, 10));
 
-            $post->save();
+            $new_user->save();
         }
     }
 }
